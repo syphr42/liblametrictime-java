ae62d1c0d6dc80539a24820b743fef1035b5a477
Gregory Moyer <moyerg@syphr.com>
2018-02-05 21:33:07 -0500

    Update license headers

--- src/main/java/org/syphr/lametrictime/api/model/enums/DisplayType.java
+++ src/main/java/org/syphr/lametrictime/api/model/enums/DisplayType.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2017 Gregory Moyer
+/**
+ * Copyright 2017-2018 Gregory Moyer and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
